793c7fa568e21e3ea6d84e219c35b0facd5d8b49
Yitian Xu <yitianxu@Yitians-MBP.home>
2017-04-02 11:08:53 +0100

    update

--- test.py
+++ test.py
@@ -5,17 +5,21 @@ from flask_testing import TestCase
 
 class TestFlaskApiUsingRequests(unittest.TestCase):
     def test_hello_world(self):
-        response = requests.get('http://localhost:5000')
-        self.assertEqual(response.json(), {'hello': 'world'})
+        response = requests.get('http://localhost:5000', content_type='html/text')
+        self.assertTrue(b'default page:' in response.data) 
 
 
 class TestFlaskApi(unittest.TestCase):
     def setUp(self):
         self.app = flaskapi.app.test_client()
 
-    def test_hello_world(self):
-        response = self.app.get('/')
-        self.assertEqual(json.loads(response.get_data()), {'hello': 'world'})
+    def test_source(self):
+        response = self.app.get('/todo/api/v1.0/countries?target=source')
+        self.assertis(json.loads(response.get_data()), [{"name": "United Kingdom", "isoCode": "GB"},{"name": "Ireland", "isoCode": "IE"}])
+    
+    def test_destination(self):
+        response = self.app.get('/todo/api/v1.0/countries?target=destination')
+        self.assertis(json.loads(response.get_data()), [ {"name": "Spain", "isoCode": "ES"},{"name": "France", "isoCode": "FR"}])
 
 
 if __name__ == "__main__":
